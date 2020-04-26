fd4548ae2f6d90b21046089ac2232137be369c53
arifrusman <arif83.ar@gmail.com>
2020-04-26 09:45:54 +0700

    inisialisasi files

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container mt-2">
+	<h1>About Me</h1>
+	<img src="<?= BASEURL; ?>/img/profile.jpg" alt="Arif Rusman" width="200" class="rounded-circle shadow">
+	<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
---
+++ app/views/about/page.php
@@ -0,0 +1,4 @@
+<div class="container mt-2">
+	<h1>My Pages</h1>
+</div>
+
